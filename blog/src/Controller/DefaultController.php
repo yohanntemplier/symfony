0c00d95cd39fadb8985dbf20329e83e8adfc8474
yohanntemplier <templieryohann@gmail.com>
2019-05-08 20:08:13 +0200

    ajout route

--- blog/src/Controller/DefaultController.php
+++ blog/src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-        public function index()
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index()
     {
-        return $this->render('/default.html.twig');
+        return $this->render('blog/default.html.twig'
+        );
     }
 }
