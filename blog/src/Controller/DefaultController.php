bf3b1968d5e7c38a35ba0abc2b4bdd7697e12cd2
yohanntemplier <templieryohann@gmail.com>
2019-05-09 07:43:16 +0200

    correction bug

--- blog/src/Controller/DefaultController.php
+++ blog/src/Controller/DefaultController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -15,7 +14,6 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
-        return $this->render('blog/default.html.twig'
-        );
+        return $this->render('default.html.twig');
     }
 }
