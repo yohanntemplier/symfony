b0894a98a18fe449297dcb062d9f2fa00a71ffa0
yohanntemplier <templieryohann@gmail.com>
2019-05-13 11:37:41 +0200

    ajout quete 6

--- blog/src/Entity/Article.php
+++ blog/src/Entity/Article.php
@@ -20,6 +20,12 @@ class Article
      * @ORM\Column(type="text")
      */
     private $content;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $category;
     public function getId(): ?int
     {
         return $this->id;
@@ -40,6 +46,18 @@ class Article
     public function setContent(string $content): self
     {
         $this->content = $content;
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
         return $this;
     }
 }
