2875919211c05d5b20a30ac9bd632ed7e4f8ef77
yohanntemplier <templieryohann@gmail.com>
2019-05-21 10:49:54 +0200

    ajout quete 13

--- blog/src/Form/ArticleType.php
+++ blog/src/Form/ArticleType.php
@@ -2,10 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Tag;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class ArticleType extends AbstractType
 {
@@ -15,7 +17,12 @@ class ArticleType extends AbstractType
             ->add('title')
             ->add('content')
             ->add('category', null, ['choice_label' => 'name'])
-            ->add('tags', null,['choice_label'    => 'name',]);
+            ->add('tags', EntityType::class, [
+            'by_reference' => false,
+            'expanded' => true,
+            'multiple' => true,
+            'class' => Tag::class,
+            'choice_label' => 'name']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
